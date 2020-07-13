a24432d371f22dfc8f1e93c459a7b07b10f2df68
Yhomi <kareemyomi91@gmail.com>
2020-07-13 13:06:02 +0100

    set view engine

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 const express = require('express');
 const passport = require('passport');
 const dotenv = require('dotenv');
+const exphbs = require('express-handlebars');
+
+dotenv.config();
+const app = express();
+
+// set view engine
+
+app.engine('.hbs',exphbs({defaultLayout:'main',extname:'.hbs'}));
+app.set('view engine','.hbs');
+
+app.get('/',(req,res)=>{
+  res.send('Hello World')
+})
+
+const PORT = process.env.PORT || 7000
+app.listen(PORT,()=>console.log(`Server listening on port ${PORT}`))
